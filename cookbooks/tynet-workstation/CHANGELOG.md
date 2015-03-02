e1215edbae0d6f148a338b103435c406b45dc5d3
Ty Alexander <ty.alexander@sendgrid.com>
2015-03-01 23:42:35 -0700

    Moved to using thor scmversion, fix broken attributes, missing ] Version bump #minor

--- cookbooks/tynet-workstation/CHANGELOG.md
+++ cookbooks/tynet-workstation/CHANGELOG.md
@@ -1,5 +1,1 @@
-# 0.1.1
-Fixup list of homebrew install
-
-# 0.1.0
-Initial release of tynet-workstation
+Please see master commit history
--- cookbooks/tynet-workstation/metadata.rb
+++ cookbooks/tynet-workstation/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ty.alexander@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tynet-workstation'
 long_description 'Installs/Configures tynet-workstation'
-version          '0.1.1'
+version          File.read("VERSION")
 
 depends 'build-essential', '~> 2.1.3'
 depends 'chef-client', '~> 4.2.4'
